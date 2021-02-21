a3219bfcaa31f82e276e7507b4f289c2e9463ab3
Vitaly <perekutnev@gmail.com>
2021-02-21 15:17:50 +0300

    abc

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,24 @@
 import Wrapper from './components/Wrapper'
 export default {
   name: 'App',
+  metaInfo: {
+    link: [
+      {
+        rel:"manifest", href: '/site.webmanifest'
+      },
+    ],
+    meta: [
+      {
+        name: 'mobile-web-app-capable', content:'yes'
+      },
+      {
+        name: 'apple-mobile-web-app-capable', content:'yes'
+      },
+      {
+        name: 'apple-mobile-web-app-title', content:'Beta'
+      }
+    ]
+  },
   components: { 
     Wrapper
   }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '../css/style.css'
-
+import VueMeta from 'vue-meta'
 Vue.config.productionTip = false
-
+Vue.use(VueMeta)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
